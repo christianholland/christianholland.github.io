60633aa6be189dbb2a580d84d572ed7090021b0b
Christian <cholland2408@gmail.com>
2020-05-07 12:39:31 +0200

    fixed homepage key

---
+++ build/precache-manifest.ecaf9a08ac925b505b4db07ff189c7b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce99c89d24b642aa8f6c99604ecd67e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7e4924a36b80f39ab6b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fbd0e9dd881bcc263cf7",
+    "url": "/static/js/2.4580527e.chunk.js"
+  },
+  {
+    "revision": "d7e4924a36b80f39ab6b",
+    "url": "/static/js/main.b3b8bfa3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a298c9d6454757395c4fb0d8419bc18c",
+    "url": "/static/media/profile.a298c9d6.jpg"
+  }
+]);
