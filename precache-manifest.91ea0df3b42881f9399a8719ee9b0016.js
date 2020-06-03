86960c11c94ebc1a1191800f33434a17989080c8
Christian <cholland2408@gmail.com>
2020-06-03 10:26:13 +0200

    Updates

---
+++ precache-manifest.91ea0df3b42881f9399a8719ee9b0016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9f088367fa71ca58ea3c32c97da1d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ac3843977133d138bf9",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ccbef7de6d1995caab2f",
+    "url": "/static/js/2.ec1c921e.chunk.js"
+  },
+  {
+    "revision": "6ac3843977133d138bf9",
+    "url": "/static/js/main.cfa216a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a298c9d6454757395c4fb0d8419bc18c",
+    "url": "/static/media/profile.a298c9d6.jpg"
+  }
+]);
